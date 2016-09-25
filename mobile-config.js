7fcccb92052b6071b0d56eaae09f33f13e7ac761
Qubit Smith <qubitsmith@gmail.com>
2016-09-25 16:07:18 +0300

    people map

---
+++ mobile-config.js
@@ -0,0 +1,5 @@
+App.accessRule('*');
+App.accessRule('https://*.googleapis.com/*');
+App.accessRule('https://*.google.com/*');
+App.accessRule('https://*.gstatic.com/*');
+App.setPreference('android-targetSdkVersion', '23');
