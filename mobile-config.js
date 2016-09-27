1952c1542707d10fe2128bbfcb10ce3537693a27
openqubit <qubitsmith@gmail.com>
2016-09-28 02:23:14 +0300

    Update mobile-config.js

--- mobile-config.js
+++ mobile-config.js
@@ -2,4 +2,4 @@ App.accessRule('*');
 App.accessRule('https://*.googleapis.com/*');
 App.accessRule('https://*.google.com/*');
 App.accessRule('https://*.gstatic.com/*');
-App.setPreference('android-targetSdkVersion', '22');
+App.setPreference('android-targetSdkVersion', '23');
