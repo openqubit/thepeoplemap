2a589755e19f29569793e8458a87968e624f920c
openqubit <qubitsmith@gmail.com>
2016-09-27 08:08:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ function onSuccess(contacts) {
          console.log(contacts[i].id + " - " + contacts[i].displayName);
          for(var j = 0; j < contacts[i].phoneNumbers.length; j++) {
             var phone = contacts[i].phoneNumbers[j];
-            console.log("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber+ ")");
-              $('.table tbody').append('<tr class="child"><td>"'+contacts[i].id+'"</td><td>"'+phone.number+'"</td><td><button type="button" class="invite btn btn-primary">Invite</button></td></tr>');
+            alert("===> " + contacts[i].id + "  " + phone.number);
+              $('.table tbody').append('<tr class="child"><td>"'+contacts[i].id+'"</td><td>"'+phone.number+'"</td><td>"'+contacts[i].displayName+'"</td><td><button type="button" class="invite btn btn-primary">Invite</button></td></tr>');
          }
       }
 }
